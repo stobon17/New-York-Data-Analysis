203ec6b2cd3c37054088cfd0fb5eca24659642b2
stobon <stobon@ufl.edu>
2020-04-17 13:45:21 -0400

    cleanup

--- dv.php
+++ dv.php
@@ -126,17 +126,6 @@ chart1 = new Chart('multiline', {
   options: options
 });
 
-/* add active class on click */
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV1");
-var btns = header.getElementsByClassName("lbtn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
 }
 function changeData1(index) {
   chart1.data.datasets.forEach(function(dataset) {
